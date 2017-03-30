4afe38855b314daa1de3adf228ee55f25fe7ec6d
Chris Thompson <christhompsontldr@gmail.com>
2017-03-30 08:59:24 -0400

    Fixing a bug where the route was not set correctly.

--- views/stop.blade.php
+++ views/stop.blade.php
@@ -1,3 +1,3 @@
-<a href="{{ config('impersonate.routes.stop') }}" id="{{ config('impersonate.btn.id', 'btn-impersonate') }}" class="{{ config('impersonate.btn.class', 'btn btn-sm btn-danger') }}" style="{{ config('impersonate.btn.style', 'position: absolute;top: 0;right: 0;') }}">
+<a href="{{ route('impersonate.stop') }}" id="{{ config('impersonate.btn.id', 'btn-impersonate') }}" class="{{ config('impersonate.btn.class', 'btn btn-sm btn-danger') }}" style="{{ config('impersonate.btn.style', 'position: absolute;top: 0;right: 0;') }}">
     <span class="{{ config('impersonate.btn.icon', 'glyphicon glyphicon-repeat') }}"></span>{{ config('impersonate.btn.text', '') }}
 </a>
