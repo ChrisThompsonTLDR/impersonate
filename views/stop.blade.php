2919898ddc2369e4861409247c6a82a7b1edf8ed
Chris Thompson <christhompsontldr@gmail.com>
2017-07-27 14:50:54 -0400

    DOM stuff doesn't need to be a config.  Make a new blade if you want to modify this.

--- views/stop.blade.php
+++ views/stop.blade.php
@@ -1,3 +1,5 @@
-<a href="{{ route('impersonate.stop') }}" id="{{ config('impersonate.btn.id', 'btn-impersonate') }}" class="{{ config('impersonate.btn.class', 'btn btn-sm btn-danger') }}" style="{{ config('impersonate.btn.style', 'position: absolute;top: 0;right: 0;') }}">
-    <span class="{{ config('impersonate.btn.icon', 'glyphicon glyphicon-repeat') }}"></span>{{ config('impersonate.btn.text', '') }}
-</a>
+<div id="impersonate">
+    <a href="{{ route('impersonate.stop') }}" class="btn btn-sm btn-danger" style="position: absolute;top: 0;right: 0;">
+        <span class="glyphicon glyphicon-repeat"></span>
+    </a>
+</div>
