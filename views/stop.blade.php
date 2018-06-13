1d63beaccc8050a77106755eff807f42fd7e90f1
Chris Thompson <lokiracer@gmail.com>
2018-06-13 00:27:15 -0400

    Don't assume Bootstrap 3 will be available.

--- views/stop.blade.php
+++ views/stop.blade.php
@@ -1,5 +1,5 @@
 <div id="impersonate">
-    <a href="{{ route('impersonate.stop') }}" class="btn btn-sm btn-danger" style="position: absolute;top: 0;right: 0;">
-        <span class="glyphicon glyphicon-repeat"></span>
+    <a href="{{ route('impersonate.stop') }}" class="btn btn-sm btn-danger" style="position: absolute;top: 0;right: 0;z-index: 99;">
+        Stop Impersonating
     </a>
 </div>
